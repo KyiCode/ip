fa842125a46a254b4eb1b12095fd53d53c27ff16
KyiCode <kaijunc23@gmail.com>
2026-01-23 00:40:03 +0800

    level 4

--- src/main/java/Meow.java
+++ src/main/java/Meow.java
@@ -6,15 +6,8 @@ public class Meow {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        String intro =
-                "____________________________________________________________\n"
-                        + "Hello! I'm Meow\n"
-                        + "What can I do for you?\n"
-                        + "____________________________________________________________\n";
-        String outro =
-                "____________________________________________________________\n"
-                        + "Bye. Hope to see you again soon!\n"
-                        + "____________________________________________________________";
+        String intro = "Hello! I'm Meow\n" + "What can I do for you?\n";
+        String outro = "Bye. Hope to see you again soon!\n";
 
         System.out.println(intro);
 
@@ -42,9 +35,67 @@ public class Meow {
                 continue;
             }
 
+            if (splittedText[0].equals("todo")) {
+                splittedText = text.split("todo ");
+                Task task = new ToDo(splittedText[1]);
+            }
 
-            Task task = new Task(text);
+            if (splittedText[0].equals("deadline")) {
+                splittedText = text.split("deadline ");
+                Task task = new Deadline(splittedText[1].split(" /by "));
+            }
+            if (splittedText[0].equals("event")) {
+                splittedText = text.split("event ");
+                splittedText = splittedText[1].split(" /from ");
+                String eventName = splittedText[0];
+                splittedText = splittedText[1].split(" /to ");
+                Task task = new Event(eventName, splittedText[0], splittedText[1]);
+            }
+
         }
+    }
+}
+class ToDo extends Task {
+    public ToDo(String taskName) {
+        super(taskName);
+        System.out.println("Added: " + this.toString());
+        System.out.println(Task.getListSize() + " tasks in list.");
+    }
+}
+
+class Deadline extends Task {
+    String deadline = " ";
+    public Deadline (String[] taskName) {
+        super(taskName[0]);
+        this.deadline = taskName[1];
+        System.out.println("Added: " + this.toString());
+        System.out.println(Task.getListSize() + " tasks in list.");
+    }
+
+    @Override
+    public String toString() {
+        String doneStatus = "[D][ ] ";
+        if (this.status()) doneStatus = "[D][X] ";
+        return doneStatus + this.getTaskName() + " (by: " + deadline +")";
+    }
+}
+
+class Event extends Task {
+    String from;
+    String to;
+    public Event (String taskName, String from, String to) {
+        super(taskName);
+        this.from = from;
+        this.to = to;
+        System.out.println("Added: " + this.toString());
+        System.out.println(Task.getListSize() + " tasks in list.");
+    }
+
+    @Override
+    public String toString() {
+        String doneStatus = "[E][ ] ";
+        if (this.status()) doneStatus = "[E][X] ";
+        return doneStatus + this.getTaskName() + " (from: " + from + " to: " + to + ")";
     }
 }
 
@@ -56,13 +107,12 @@ class Task {
     public Task (String taskName) {
         this.taskName = taskName;
         list.add(this);
-        System.out.println("added: " + taskName);
     }
 
     public static void markDone(int taskIndex) {
         Task task = list.get(taskIndex - 1);
         task.done = true;
-        System.out.println("Nice! I've marked this task as done: \n");
+        System.out.println("Nice! I've marked this task as done:");
         System.out.println(task.toString());
     }
 
@@ -73,6 +123,10 @@ class Task {
         System.out.println(task.toString());
     }
 
+    public static int getListSize() {
+        return list.size();
+    }
+
     public static void getTaskList() {
         int count = 1;
         for (Task x : list) {
@@ -81,10 +135,18 @@ class Task {
         }
     }
 
+    public boolean status() {
+        return this.done;
+    }
+
+    public String getTaskName() {
+        return this.taskName;
+    }
+
     @Override
     public String toString() {
-        String doneStatus = "[ ] ";
-        if (this.done) doneStatus = "[X] ";
+        String doneStatus = "[T][ ] ";
+        if (this.done) doneStatus = "[T][X] ";
         return doneStatus + this.taskName;
     }
 }
