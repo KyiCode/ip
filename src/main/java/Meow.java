660b5c94819a2c5394ebe582d294a70b2cfd221d
KyiCode <kaijunc23@gmail.com>
2026-01-29 01:27:25 +0800

    implement level 7 partially

--- src/main/java/Meow.java
+++ src/main/java/Meow.java
@@ -1,11 +1,26 @@
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Objects;
 import java.util.Scanner;
 
 public class Meow {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Brain meowmeow = new Brain();
         Scanner sc = new Scanner(System.in);
+
+        Path dataDir = Paths.get("ip/data");
+        Path filePath = Paths.get("ip/data/meow.txt");
+
+        if (!Files.exists(dataDir)) {
+            Files.createDirectories(dataDir);
+            Files.createFile(filePath);
+        }
+        if (!Files.exists(filePath)) {
+            Files.createFile(filePath);
+        }
 
         while (sc.hasNext()) {
             String text = sc.nextLine();
@@ -27,7 +42,7 @@ class Brain {
         System.out.println(intro);
     }
 
-    public void thinking(String text) throws InvalidCommandException, InvalidMarkingException {
+    public void thinking(String text) throws InvalidCommandException, InvalidMarkingException, IOException {
         String[] splittedText = text.split(" ");
 
         if (text.equals("bye")) {
@@ -81,7 +96,7 @@ class Brain {
 }
 
 class ToDo extends Task {
-    public ToDo(String taskName) throws InvalidCommandException {
+    public ToDo(String taskName) throws InvalidCommandException, IOException {
         super(taskName);
         System.out.println("Added: " + this.toString());
         System.out.println(Task.getListSize() + " tasks in list.");
@@ -90,7 +105,7 @@ class ToDo extends Task {
 
 class Deadline extends Task {
     String deadline = " ";
-    public Deadline (String[] taskName) throws InvalidCommandException{
+    public Deadline (String[] taskName) throws InvalidCommandException, IOException {
         super(taskName[0]);
         this.deadline = taskName[1];
         if (this.deadline == null) throw new NullDateException();
@@ -109,7 +124,7 @@ class Deadline extends Task {
 class Event extends Task {
     String from;
     String to;
-    public Event (String taskName, String from, String to) throws InvalidCommandException {
+    public Event (String taskName, String from, String to) throws InvalidCommandException, IOException {
         super(taskName);
         this.from = from;
         this.to = to;
@@ -132,37 +147,43 @@ class Task {
     private static ArrayList<Task> list = new ArrayList<>();
     private boolean done = false;
 
-    public Task (String taskName) throws NullTaskDescriptionException {
+    public Task (String taskName) throws NullTaskDescriptionException, IOException {
         this.taskName = taskName;
         if (this.taskName == null) {
             throw new NullTaskDescriptionException();
         }
         list.add(this);
+        FileWriter fw = new FileWriter("ip/data/meow.txt", true);
+        fw.write(this.toString() + System.lineSeparator());
+        fw.close();
     }
 
-    public static void markDone(int taskIndex) throws InvalidMarkingException {
+    public static void markDone(int taskIndex) throws InvalidMarkingException, IOException {
         if (taskIndex > list.size()) throw new InvalidMarkingException();
         Task task = list.get(taskIndex - 1);
         task.done = true;
         System.out.println("Nice! I've marked this task as done:");
         System.out.println(task.toString());
+        FileOperator.markOperation(taskIndex, task.toString());
     }
 
-    public static void markUndone(int taskIndex) throws InvalidMarkingException {
+    public static void markUndone(int taskIndex) throws InvalidMarkingException, IOException {
         if (taskIndex > list.size()) throw new InvalidMarkingException();
         Task task = list.get(taskIndex - 1);
         task.done = false;
         System.out.println("I've marked this task as not done yet:");
         System.out.println(task.toString());
+        FileOperator.markOperation(taskIndex, task.toString());
     }
 
-    public static void delete(int taskIndex) throws InvalidMarkingException {
+    public static void delete(int taskIndex) throws InvalidMarkingException, IOException {
         if (taskIndex > list.size()) throw new InvalidMarkingException();
         Task task = list.get(taskIndex - 1);
         list.remove(taskIndex - 1);
         System.out.println("I've eaten this task:");
         System.out.println(task.toString());
         System.out.println(Task.getListSize() + " tasks in list.");
+        FileOperator.delOperation(taskIndex);
     }
 
     public static int getListSize() {
@@ -196,6 +217,52 @@ class Task {
         return doneStatus + this.taskName;
     }
 }
+
+class FileOperator {
+    public static void markOperation(int index, String text) throws IOException {
+        Scanner sc = new Scanner(new FileReader("ip/data/meow.txt"));
+        FileWriter fw = new FileWriter("ip/data/temp.txt", true);
+        int count = 0;
+
+        while (sc.hasNext()) {
+            String stringLine = sc.nextLine();
+            if (count + 1 == index) {
+                stringLine = text;
+            }
+            count+= 1;
+            fw.write(stringLine + System.lineSeparator());
+        }
+        sc.close();
+        fw.close();
+
+        Files.delete(Paths.get("ip/data/meow.txt"));
+        Files.move(Paths.get("ip/data/temp.txt"), Paths.get("ip/data/meow.txt"));
+    }
+
+    public static void delOperation(int index) throws IOException {
+        Scanner sc = new Scanner(new FileReader("ip/data/meow.txt"));
+        FileWriter fw = new FileWriter("ip/data/temp.txt", true);
+        int count = 0;
+
+        while (sc.hasNext()) {
+            String stringLine = sc.nextLine();
+            if (count + 1 == index) {
+                count+= 1;
+                continue;
+            }
+            count+= 1;
+            fw.write(stringLine + System.lineSeparator());
+        }
+        sc.close();
+        fw.close();
+
+        Files.delete(Paths.get("ip/data/meow.txt"));
+        Files.move(Paths.get("ip/data/temp.txt"), Paths.get("ip/data/meow.txt"));
+    }
+
+}
+
+
 
 class InvalidCommandException extends Exception {
     public InvalidCommandException() {
