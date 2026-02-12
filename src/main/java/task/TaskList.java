6f1f4052d50e540ed53e52877ed2c322b7ca8a4d
KyiCode <kaijunc23@gmail.com>
2026-02-12 23:45:03 +0800

    add bug fix.

--- src/main/java/task/TaskList.java
+++ src/main/java/task/TaskList.java
@@ -27,7 +27,7 @@ public class TaskList {
      * @throws InvalidMarkingException If marking operation on invalid task index.
      */
     public static Task markDone(int taskIndex) throws InvalidMarkingException {
-        if (isValidIndex(taskIndex)) {
+        if (!isValidIndex(taskIndex)) {
             throw new InvalidMarkingException();
         }
         Task task = list.get(taskIndex - 1);
@@ -43,7 +43,7 @@ public class TaskList {
      * @throws InvalidMarkingException If marking operation on invalid task index.
      */
     public static Task markUndone(int taskIndex) throws InvalidMarkingException {
-        if (isValidIndex(taskIndex)) {
+        if (!isValidIndex(taskIndex)) {
             throw new InvalidMarkingException();
         }
         Task task = list.get(taskIndex - 1);
@@ -63,7 +63,7 @@ public class TaskList {
      * @throws InvalidMarkingException If delete operation on invalid task index.
      */
     public static Task delete(int taskIndex) throws InvalidMarkingException {
-        if (isValidIndex(taskIndex)) {
+        if (!isValidIndex(taskIndex)) {
             throw new InvalidMarkingException();
         }
         Task task = list.get(taskIndex - 1);
